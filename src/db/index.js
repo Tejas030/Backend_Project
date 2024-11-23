7b5eca03accc06daaeed52df0ca976a5e278c688
Tejas030 <tejasshroff0303@gmail.com>
2024-11-23 10:29:22 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connect_DB = async()=>
 {
     try {
        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
-       console.log(`MongoDB connected || DB Host : ${connectionInstance.connection.host}`)
+       console.log(`MongoDB connected !! DB Host : ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("Error :", error)
         process.exit(1)
